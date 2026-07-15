1dd0c716a07836653cfb165ffdd010ae1e27e2fd
your father <pixelvortex56@gmail.com>
2026-07-15 08:50:45 +0600

    slight edit

--- init.h
+++ init.h
@@ -1,13 +1,7 @@
 #ifndef INIT_H
 #define INIT_H
 
-/* =========================================================
-   init.h
-   ------
-   Declarations for the game initialization functions.
-   init_game() resets everything for a fresh start.
-   init_kitchen() sets up the kitchen grid and clue tiles.
-   ========================================================= */
+
 
 #include "globals.h"
 #include "utils.h"
